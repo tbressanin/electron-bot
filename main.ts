2a56a138685b0d20b2de9139afd1d8d8e3b58ec2
bressawsk <thiago@freecode.com.br>
2021-06-13 19:46:56 -0300

    Add lint profiles

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow } from "electron";
+import path from "path";
 
-app.on('ready', () => {
-	let win = new BrowserWindow()
-	win.setBounds({width: 600, height: 100 })
-}) 
+app.on("ready", async () => {
+	let win = new BrowserWindow({
+		autoHideMenuBar: true,
+		title: "Electron Training",
+	});
+
+	const indexHTML = path.join(__dirname + "/index.html");
+
+	await win.loadFile(indexHTML);	
+
+	win.setBounds({ width: 600, height: 300 });
+});
